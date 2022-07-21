498bbb1ef38513117aa289176b2335cb1f378372
lewinsda <lewinsda@metal-1-0.local>
2022-07-21 13:47:28 -0700

    more changes

--- test_arm.py
+++ test_arm.py
@@ -11,7 +11,7 @@ import sys
 
 """Methods for testing arms"""
 
-def test_label_prop(datasets, maskings, arm, epochs=100, repeats=3):
+def test_label_prop(datasets, maskings, arm, epochs=100, repeats=3, batch_size=35):
     """method testing label propagation for any amount of datasets and maskings
         datasets: [(X,y),(X,y),...]
         maskings: [.2, .3,...]
@@ -23,7 +23,7 @@ def test_label_prop(datasets, maskings, arm, epochs=100, repeats=3):
         X = dataset[0]
         y = dataset[1]
 
-        X = utilities.preprocess(X)
+        X = utilities.preprocess(X, scale=False)
 
         dataset_results = []
         for mask in maskings:
@@ -32,8 +32,8 @@ def test_label_prop(datasets, maskings, arm, epochs=100, repeats=3):
             train_dataset = torch.utils.data.TensorDataset(torch.tensor(X), torch.tensor(masked_y))
             test_dataset = torch.utils.data.TensorDataset(torch.tensor(X), torch.tensor(y))
 
-            train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=90, shuffle=True)
-            test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=90, shuffle=False)
+            train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+            test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
            
             accuracies = []
             cms = []
@@ -56,14 +56,29 @@ def test_label_prop(datasets, maskings, arm, epochs=100, repeats=3):
     return all_results
             
 
+def make_plot(results, neighbors):
+    """makes plot of dataset vs accuracy showing masking differences"""
+    results = np.array(results, dtype=object)
+    results_df = pd.DataFrame({"dataset":np.repeat(list(range(len(results))), len(results[0])),"masking":results[:,:,0].flatten(), "accuracy":results[:,:,1].flatten()})
+    results_df["dataset"] = results_df["dataset"].astype("string")
+
+    plt.scatter(results_df['dataset'], results_df['accuracy'], c=results_df['masking'], cmap=plt.cm.viridis)
+    plt.xlabel("Dataset")
+    plt.ylabel("Accuracy after training 150 epochs")
+    clb = plt.colorbar()
+    clb.ax.set_title('Masking %')
+    plt.title("Label Propagation by Dataset: n=" + str(neighbors))
+    plt.grid(axis="y")
+    plt.savefig("n="+str(neighbors)+".pdf", format="pdf")
+
 if __name__ == "__main__":
     
-    output_path = sys.argv[1]
-    neighbors = int(sys.argv[2])
+    #output_path = sys.argv[1]
+    neighbors = int(sys.argv[1])
     datasets = []
     print(neighbors)
     dataset_names = []
-    for i in np.arange(.4,.5,.1):
+    for i in np.arange(.1,.5,.1):
         data_path = "simulations/splat_" + str(round(i,5)) + "_de/counts.csv"
         meta_path = "simulations/splat_" + str(round(i,5)) + "_de/meta.csv"
         temp_data = pd.read_csv(data_path, index_col=0)
@@ -74,10 +89,11 @@ if __name__ == "__main__":
         datasets.append((X,y))
         dataset_names.append(str(round(i,5)))
 
-    maskings = [.5,.6,.7, .8, .9, .95, .99]
-    arm = LabelProp("configs/semi_basic.txt", neighbors)
-    results = test_label_prop(datasets, maskings, arm, epochs=200)
-
+    maskings = [.6,.7, .8, .9, .95, .99]
+    arm = LabelProp("configs/semi_basic_linear.txt", neighbors)
+    results = test_label_prop(datasets, maskings, arm, epochs=150)
+    make_plot(results, neighbors)
+    """
     x = 0
     for dataset_results in results:
         dir_path = output_path + "_dataset_" + dataset_names[x]
@@ -105,3 +121,4 @@ if __name__ == "__main__":
             #plt.savefig((dir_path + "/confusion_matrix_" + str(masking[0])))
         
         x +=1
+    """
--- test_arm.sh
+++ test_arm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --mem=32g
-#SBATCH --array=2,5,10,15,20,30,40,50
+#SBATCH --array=2,5,10,15
 #SBATCH --partition=gpu
 #SBATCH --time=1:00:00
 #SBATCH --output=log-%j.out
 #SBATCH --error=log-%j.err
 #SBATCH --gres=gpu:1
 
-python -u test_arm.py /home/groups/ConradLab/daniel/0.4_marm_test/output_${SLURM_ARRAY_TASK_ID} ${SLURM_ARRAY_TASK_ID}
+python -u test_arm.py ${SLURM_ARRAY_TASK_ID}
